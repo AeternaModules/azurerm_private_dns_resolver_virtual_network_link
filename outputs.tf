03215d1d6cd005509dcc6bcacbbffd5c482088b4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:48 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "private_dns_resolver_virtual_network_links" {
-  description = "All private_dns_resolver_virtual_network_link resources"
-  value       = azurerm_private_dns_resolver_virtual_network_link.private_dns_resolver_virtual_network_links
-}
 output "private_dns_resolver_virtual_network_links_dns_forwarding_ruleset_id" {
-  description = "List of dns_forwarding_ruleset_id values across all private_dns_resolver_virtual_network_links"
-  value       = [for k, v in azurerm_private_dns_resolver_virtual_network_link.private_dns_resolver_virtual_network_links : v.dns_forwarding_ruleset_id]
+  description = "Map of dns_forwarding_ruleset_id values across all private_dns_resolver_virtual_network_links, keyed the same as var.private_dns_resolver_virtual_network_links"
+  value       = { for k, v in azurerm_private_dns_resolver_virtual_network_link.private_dns_resolver_virtual_network_links : k => v.dns_forwarding_ruleset_id }
 }
 output "private_dns_resolver_virtual_network_links_metadata" {
-  description = "List of metadata values across all private_dns_resolver_virtual_network_links"
-  value       = [for k, v in azurerm_private_dns_resolver_virtual_network_link.private_dns_resolver_virtual_network_links : v.metadata]
+  description = "Map of metadata values across all private_dns_resolver_virtual_network_links, keyed the same as var.private_dns_resolver_virtual_network_links"
+  value       = { for k, v in azurerm_private_dns_resolver_virtual_network_link.private_dns_resolver_virtual_network_links : k => v.metadata }
 }
 output "private_dns_resolver_virtual_network_links_name" {
-  description = "List of name values across all private_dns_resolver_virtual_network_links"
-  value       = [for k, v in azurerm_private_dns_resolver_virtual_network_link.private_dns_resolver_virtual_network_links : v.name]
+  description = "Map of name values across all private_dns_resolver_virtual_network_links, keyed the same as var.private_dns_resolver_virtual_network_links"
+  value       = { for k, v in azurerm_private_dns_resolver_virtual_network_link.private_dns_resolver_virtual_network_links : k => v.name }
 }
 output "private_dns_resolver_virtual_network_links_virtual_network_id" {
-  description = "List of virtual_network_id values across all private_dns_resolver_virtual_network_links"
-  value       = [for k, v in azurerm_private_dns_resolver_virtual_network_link.private_dns_resolver_virtual_network_links : v.virtual_network_id]
+  description = "Map of virtual_network_id values across all private_dns_resolver_virtual_network_links, keyed the same as var.private_dns_resolver_virtual_network_links"
+  value       = { for k, v in azurerm_private_dns_resolver_virtual_network_link.private_dns_resolver_virtual_network_links : k => v.virtual_network_id }
 }
 
