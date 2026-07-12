cfa9d2260b6c6ccd20af9e04023e0abf3016bc4a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "private_dns_resolver_virtual_network_links_id" {
+  description = "Map of id values across all private_dns_resolver_virtual_network_links, keyed the same as var.private_dns_resolver_virtual_network_links"
+  value       = { for k, v in azurerm_private_dns_resolver_virtual_network_link.private_dns_resolver_virtual_network_links : k => v.id }
+}
 output "private_dns_resolver_virtual_network_links_dns_forwarding_ruleset_id" {
   description = "Map of dns_forwarding_ruleset_id values across all private_dns_resolver_virtual_network_links, keyed the same as var.private_dns_resolver_virtual_network_links"
   value       = { for k, v in azurerm_private_dns_resolver_virtual_network_link.private_dns_resolver_virtual_network_links : k => v.dns_forwarding_ruleset_id }
